40b55f6aa1ab8d3998d23176d84f77d999e2b001
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-05-04 20:07:54 +0530

    code

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 '''Image Captcha'''
 
 # Importing Necessary module
-from captcha.image import ImageCaptcha, _Captcha
+from captcha.image import ImageCaptcha
 
 # set image dimension
 Captcha_image = ImageCaptcha(width = 280, height = 90)
