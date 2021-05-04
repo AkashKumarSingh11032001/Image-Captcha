dab30286c667a93470e4160804a724470da67959
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-05-04 20:03:55 +0530

    code

--- code.py
+++ code.py
@@ -8,3 +8,5 @@ Captcha_image = ImageCaptcha(width = 280, height = 90)
 
 # random genrate image having alphabet and number
 values = Captcha_image.generate('12open78source6')
+
+# genrate and create the new captcha image file
